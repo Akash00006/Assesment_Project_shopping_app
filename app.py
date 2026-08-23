64996229176230e6bcd36ac6ba5f63e6f00f887c
Akash Dutta <akashkumardutta7@gmail.com>
2026-08-23 21:38:06 +0530

    Task 3: Implement Password Manager endpoints

--- app.py
+++ app.py
@@ -14,7 +14,33 @@ def health():
 def  cart():
     return 'Cart is empty.'
 
+passwords_db = {}
 
+
+@app.route('/add', methods=['POST'])
+def add_password():
+    data = request.get_json()
+    
+    
+    if not data or 'username' not in data or 'password' not in data:
+        return jsonify({"error": "Missing 'username' or 'password' in request body"}), 400
+    
+    username = data['username']
+    password = data['password']
+    
+    
+    passwords_db[username] = password
+    return jsonify({"message": f"Password saved successfully for '{username}'"}), 201
+
+
+@app.route('/get/<username>', methods=['GET'])
+def get_password(username):
+    
+    if username in passwords_db:
+        return jsonify({"username": username, "password": passwords_db[username]}), 200
+    
+    
+    return jsonify({"error": "Username not found"}), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
